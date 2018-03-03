0cf13eaec957b45f9ff2e2069b48440190399e94
jldeleage <jldeleage@10.0.187.230>
2018-03-03 14:09:09 +0100

    EnhancedPostconditions have been... enhanced !
    The properties initialUsedProperties, finalUsedProperties and
    definedProperty have been added.
    These properties are intented to help sorting
     the conditions.

--- ete/xlang/xlang-builder/src/main/java/fr/insset/jeanluc/action/semantics/builder/EnhancedPostCondition.java
+++ ete/xlang/xlang-builder/src/main/java/fr/insset/jeanluc/action/semantics/builder/EnhancedPostCondition.java
@@ -2,10 +2,11 @@
 
 package fr.insset.jeanluc.action.semantics.builder;
 
-import fr.insset.jeanluc.ete.meta.model.constraint.Condition;
 import fr.insset.jeanluc.ete.meta.model.constraint.Postcondition;
-import fr.insset.jeanluc.ete.meta.model.constraint.impl.ConditionImpl;
 import fr.insset.jeanluc.ete.meta.model.constraint.impl.PostconditionImpl;
+import fr.insset.jeanluc.ete.meta.model.emof.MofProperty;
+import java.util.LinkedList;
+import java.util.List;
 
 /**
  *
@@ -14,5 +15,36 @@ import fr.insset.jeanluc.ete.meta.model.constraint.impl.PostconditionImpl;
 public class EnhancedPostCondition extends PostconditionImpl implements Postcondition {
 
 
+    public MofProperty getDefinedProperty() {
+        return definedProperty;
+    }
+
+    public void setDefinedProperty(MofProperty definedProperty) {
+        this.definedProperty = definedProperty;
+    }
+
+    public List<MofProperty> getFinalUsedValues() {
+        return finalUsedValues;
+    }
+
+    public void setFinalUsedValues(List<MofProperty> finalUsedValues) {
+        this.finalUsedValues = finalUsedValues;
+    }
+
+    public List<MofProperty> getInitialUsedValues() {
+        return initialUsedValues;
+    }
+
+    public void setInitialUsedValues(List<MofProperty> initialUsedValues) {
+        this.initialUsedValues = initialUsedValues;
+    }
+
+
+
+
+
+    private     MofProperty             definedProperty;
+    private     List<MofProperty>       finalUsedValues = new LinkedList<>();
+    private     List<MofProperty>       initialUsedValues = new LinkedList<>();
 
 }
