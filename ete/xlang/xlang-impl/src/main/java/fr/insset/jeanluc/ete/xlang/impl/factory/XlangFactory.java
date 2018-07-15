f1ce677980fbda59a1c68c9f9df512f3bc72795c
jldeleage <jldeleage@10.0.187.230>
2018-07-15 22:26:48 +0200

    The TreeBuilder of the natural language archetype has really no more bugs
    The project needs to be simplified and TESTED.
    Sub expressions should not work.

--- ete/xlang/xlang-impl/src/main/java/fr/insset/jeanluc/ete/xlang/impl/factory/XlangFactory.java
+++ ete/xlang/xlang-impl/src/main/java/fr/insset/jeanluc/ete/xlang/impl/factory/XlangFactory.java
@@ -25,16 +25,16 @@ public class XlangFactory {
                     fr.insset.jeanluc.ete.xlang.impl.WhileDoLoopImpl.class);        
         registry.registerDefaultFactory("if_statement",
                     fr.insset.jeanluc.ete.xlang.impl.ConditionalImpl.class);        
-        registry.registerDefaultFactory("allocation",
-                    fr.insset.jeanluc.ete.xlang.impl.AllocationImpl.class);        
-        registry.registerDefaultFactory("do_while_statement",
-                    fr.insset.jeanluc.ete.xlang.impl.DoWhileLoopImpl.class);        
-        registry.registerDefaultFactory("variable_declaration_statement",
-                    fr.insset.jeanluc.ete.xlang.impl.VariableDeclarationImpl.class);        
         registry.registerDefaultFactory("assignment_statement",
                     fr.insset.jeanluc.ete.xlang.impl.AssignmentImpl.class);        
+        registry.registerDefaultFactory("do_while_statement",
+                    fr.insset.jeanluc.ete.xlang.impl.DoWhileLoopImpl.class);        
+        registry.registerDefaultFactory("allocation",
+                    fr.insset.jeanluc.ete.xlang.impl.AllocationImpl.class);        
         registry.registerDefaultFactory("method_invocation_statement",
                     fr.insset.jeanluc.ete.xlang.impl.MethodInvocationImpl.class);        
+        registry.registerDefaultFactory("variable_declaration_statement",
+                    fr.insset.jeanluc.ete.xlang.impl.VariableDeclarationImpl.class);        
         registry.registerDefaultFactory("for_statement",
                     fr.insset.jeanluc.ete.xlang.impl.ForLoopImpl.class);        
 
