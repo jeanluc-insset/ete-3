a759f5ae6f5ac5072d5e09e9c3c6030304477cc4
jldeleage <jldeleage@10.0.187.230>
2019-02-10 09:30:16 +0100

    Add CORSResponseFilter to insset-airways
    In order to provide a React client on another port (or host) we need
    to implement CORS.

---
+++ samples/insset-airways/src/main/webapp/precache-manifest.14e9ef39ebf4d82d0a8e6ee8c3ef9d6c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "77f60da19b71592a1c2c",
+    "url": "./static/js/main.77f60da1.chunk.js"
+  },
+  {
+    "revision": "13eeb2032981bf3107f9",
+    "url": "./static/js/1.13eeb203.chunk.js"
+  },
+  {
+    "revision": "77f60da19b71592a1c2c",
+    "url": "./static/css/main.8f6b996e.chunk.css"
+  },
+  {
+    "revision": "2e9041851a3466b4ca2c9c52b8b79e5e",
+    "url": "./index.html"
+  }
+];
