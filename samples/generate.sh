1f9827a3350a7d7aea3f0ef1d87c26735cd3f227
jldeleage <jldeleage@10.0.187.230>
2019-02-20 08:50:10 +0100

    Add a script to help build samples from ete-jsf-archetype
    The script provides all the data but the artifact id

---
+++ samples/generate.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+
+mvn archetype:generate -DarchetypeArtifactId=ete-jsf-archetype -DarchetypeGroupId=fr.insset.jeanluc -DarchetypeVersion=3.1 -Dversion=3.1 -Dpackage=fr.upjv.mis.ete.samples -DgroupId=fr.upjv.mis
